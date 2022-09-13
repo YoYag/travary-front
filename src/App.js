314d96f1bc9133578d845cd0a7ccacc97cb9d155
YoYag <la2043432@gmail.com>
2022-09-13 09:55:49 +0900

    시작

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>안녕...</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
