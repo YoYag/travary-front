71345fc1895fbd73002459a0a601ff92a2bbf674
YoYag <la2043432@gmail.com>
2022-09-20 11:40:14 +0900

    로그인 페이지 수정

--- src/routes/Join.js
+++ src/routes/Join.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const Login = () => {
+const Join = () => {
   return (
-    <div className="text-center my-4">
-      <h1 className="text-2xl font-bold">로그인 페이지</h1>
-      <form action="">
+    <div>
+      <form className="text-center my-20" action="">
         <label className="input-group input-group-sm my-4">
           <span className="w-16">ID</span>
           <input
             type="text"
             name="username"
             placeholder="ID를 입력해주세요"
-            className="input input-bordered input-sm w-full"
+            className="input input-bordered input-sm"
           />
         </label>
         <label className="input-group input-group-sm my-4">
@@ -20,16 +19,14 @@ const Login = () => {
             type="password"
             name="password"
             placeholder="Password를 입력해주세요"
-            className="input input-bordered input-sm w-full"
+            className="input input-bordered input-sm"
           />
         </label>
-        <div className="form-control">
-          <button className="btn my-2">로그인</button>
-          <button className="btn my-2">회원가입</button>
-        </div>
+        <button className="btn mx-4">로그인</button>
+        <button className="btn mx-4">회원가입</button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Join;
